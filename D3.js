67a2b5e996ca9947f94c760359f81f81881ff816
Drol <najklord@gmail.com>
2013-05-22 18:35:22 +0200

    AngularJS rewrite.

--- D3.js
+++ D3.js
@@ -1,35 +1,85 @@
-angular.module('D3', [])
+var d3App = angular.module('diablo3App', []);
 
-function D3Controller($scope) {
-    // Tooltiphack
-    var b = Bnet.D3.Tooltips;
-    b.registerDataOld = b.registerData;
-    b.registerData = function(data) {
-    var c = document.body.children, s = c[c.length-1].src;
-        data.params.key=s.substr(0,s.indexOf('?')).substr(s.lastIndexOf('/')+1);
-        this.registerDataOld(data);
-    }
-}
+d3App.config(function ($routeProvider) {
+    $routeProvider.
+        when('/heroes', { templateUrl: 'hero-list.html', controller: 'ProfileController' }).
+        when('/heroes/:heroId', { templateUrl: 'hero-detail.html', controller: 'HeroDetailController' }).
+        otherwise({ redirectTo: '/heroes' });
+});
 
-function ProfileController($scope, $http) {
-    $scope.profileId = 'Drol-1245';
+d3App.factory('profileFactory', function ($http, $q, $rootScope) {
+    var factory = {};
 
-    $scope.fetch = function () {
-        var url = 'http://eu.battle.net/api/d3/profile/' + $scope.profileId + '/?callback=JSON_CALLBACK&name=profiles';
+    factory.getProfile = function (profileId) {
+        var url = 'http://eu.battle.net/api/d3/profile/' + profileId + '/?callback=JSON_CALLBACK&name=profiles';
+        var def = $q.defer();
 
         $http.jsonp(url)
             .success(function (data, status) {
-                $scope.profile = data;
+                def.resolve(data);
+                console.log(data);
+            })
+            .error(function (data, status) {
+                return data || "Request failed";
+                //$scope.status = status;
+            });
+
+        return def.promise;
+    };
+
+    return factory;
+});
+
+d3App.controller('D3Controller', function ($scope) {
+    // Tooltiphack
+    var b = Bnet.D3.Tooltips;
+    b.registerDataOld = b.registerData;
+    b.registerData = function (data) {
+        var c = document.body.children, s = c[c.length - 1].src;
+        data.params.key = s.substr(0, s.indexOf('?')).substr(s.lastIndexOf('/') + 1);
+        this.registerDataOld(data);
+    }
+});
+
+d3App.controller('ProfileController', function ($scope, profileFactory) {
+    $scope.profileId = 'Drol-1245';
+
+    $scope.fetch = function () {
+        $scope.profile = profileFactory.getProfile($scope.profileId);
+
+        //var url = 'http://eu.battle.net/api/d3/profile/' + $scope.profileId + '/?callback=JSON_CALLBACK&name=profiles';
+
+        //$http.jsonp(url)
+        //    .success(function (data, status) {
+        //        $scope.profile = data;
+        //        $scope.status = status;
+        //    })
+        //    .error(function (data, status) {
+        //        $scope.profile = data || "Request failed";
+        //        $scope.status = status;
+        //    });
+    };
+});
+
+d3App.controller('HeroListController', function ($scope, $http) {
+    $scope.heroes = new Array();
+
+    $scope.fetch = function (profileId, heroId) {
+        var url = 'http://eu.battle.net/api/d3/profile/' + profileId + '/hero/' + heroId + '/?callback=JSON_CALLBACK&name=profiles';
+
+        $http.jsonp(url)
+            .success(function (data, status) {
+                $scope.heroes[heroId] = data;
                 $scope.status = status;
             })
             .error(function (data, status) {
-                $scope.profile = data || "Request failed";
+                $scope.heroes[heroId] = data || "Request failed";
                 $scope.status = status;
             });
-    },
+    };
+});
 
-    $scope.fetch();
-};
+d3App.controller('HeroDetailController', function ($scope, $routeParams) {
+    $scope.heroId = $routeParams.heroId;
+});
 
-
-//ProfileController.$inject = ['$scope'];
